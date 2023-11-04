2c86a223aa3053acb3578ceed2525ae027d26c91
PaceCodes <jerrysamuel81@gmail.com>
2023-11-04 20:36:59 +0100

    add

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Typed from 'react-typed';
 // import { render } from 'react-dom';
+import { TypeAnimation } from 'react-type-animation';
+    
 
 const Hero = () => {
   return (
@@ -12,12 +13,23 @@ const Hero = () => {
         <h1 className='text-6xl md:7xl lg:text-4xl mb-2 font-bold md:py-6'>Hire the <span className='text-[#9a00df]'>Best</span> lawyer</h1>
        
         <div className='text-white items-center text-xl font-bold'>
-        <Typed
-        strings={['We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.',
-                'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.', 
-                'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.']}
-                    typeSpeed={80} backSpeed={140} loop
-        /> </div> 
+            <TypeAnimation
+          sequence={[
+            'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.>',
+            1000,
+            'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.>',
+            1000,
+            'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.>',
+            1000,
+            'We Connect Clients To The Best Lawyers, Making Justice Accessible To Everyone.>',
+            1000
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: '1.5em', display: 'inline-block' }}
+          repeat={Infinity}
+        /> 
+        </div> 
         <button className='rounded-xl text-black bg-[#9a00df] py-4 mt-7 w-[200px] font-medium my-6 mx-auto px-5' >Get Started</button>
         </div>
         
